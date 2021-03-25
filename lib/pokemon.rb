af1ad49f82bd2ed8dd9d0636c1145ddb6f199662
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-25 16:37:53 -0400

    commit tree acd2c3ada93ff8f286e5a8e3997e1e13bd6352d1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+
+    end
+
+    def self.find(id, db)
+        found = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+        Pokemon.new(id: id, name: found[1], type: found[2], db: db)
+    end
+    def self.all
+
+    end
 end
